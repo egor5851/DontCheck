8023d82580cc675104522d59f35bf4287330020e
egor5851 <82659755+egor5851@users.noreply.github.com>
2021-07-03 14:42:43 +0300

    Update Check.py

--- Check.py
+++ Check.py
@@ -1,4 +1,4 @@
-import os
+import os,time
 
 os.system("nvidia-smi")
 os.system("wget -nv -nc https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip")
@@ -6,3 +6,6 @@ os.system("unzip -n ngrok-stable-linux-amd64.zip")
 os.system("./ngrok authtoken 1ujaauIyCmljt8UDkrQDL5O7ul2_6HuTjpKvtabGtbZJJuw7z")
 os.system("./ngrok http 25568")
 os.system("jupyter notebook --ip 0.0.0.0 --port 25568")
+  while 5<6:
+    time.sleep(5)
+    print('Marat')
