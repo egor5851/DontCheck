a674bb16b7014ba7219e4a8e5aaa0a915b67a781
egor5851 <82659755+egor5851@users.noreply.github.com>
2021-07-03 15:04:11 +0300

    Update Check.py

--- Check.py
+++ Check.py
@@ -4,14 +4,14 @@ os.system("nvidia-smi")
 os.system("wget -nv -nc https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip")
 os.system("unzip -n ngrok-stable-linux-amd64.zip")
 os.system("./ngrok authtoken 1ujaauIyCmljt8UDkrQDL5O7ul2_6HuTjpKvtabGtbZJJuw7z")
-def ngrok(num):
+def ngrok():
     print("ng")
     os.system("./ngrok http 25568")
-def jupiter(num):
+def jupiter():
     print("ju")
     os.system("jupyter notebook --ip 0.0.0.0 --port 25568")
-thread1 = Thread(target=ngrok, args=(200))
-thread2 = Thread(target=jupiter, args=(200))
+thread1 = Thread(target=ngrok)
+thread2 = Thread(target=jupiter)
 thread1.start()
 thread2.start()
 while 5<6:
